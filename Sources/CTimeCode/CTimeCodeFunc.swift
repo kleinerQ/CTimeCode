a9b8bf519eefcac74b53780c72c4726ebb2885ca
chihyen <chih.yen5408@gmail.com>
2020-07-23 14:12:10 +0800

    add

--- Sources/CTimeCode/CTimeCodeFunc.swift
+++ Sources/CTimeCode/CTimeCodeFunc.swift
@@ -1,14 +1,14 @@
 
 public class CTimeCodeFunc {
-    var ptr: CTimeCode?
+    public var ptr: UnsafeMutablePointer<LTCDecoder>
 
     // Creates a new bitmap (initially empty)
     public init() {
-        self.ptr = CTimeCode()
+        self.ptr = Cltc.ltc_decoder_create(20,20)
     }
   
-    // Add "value" inside the bitmap
-    public func test()->String {
-        return self.ptr?.text ?? ""
-    }
+//    // Add "value" inside the bitmap
+//    public func test()->String {
+//        return self.ptr?.text ?? ""
+//    }
 }
